e55b23ed65645d61fe20e7a378674ec376da9dda
Innapropiate <38080090+Innapropiate@users.noreply.github.com>
2019-05-25 01:52:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.MkYfv3_d6LzTTmM-2rNhMrjh12TXnWfm);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
